8af6f5e40795a973a48a24e106f9626f1c569bec
Joao Albuquerque <joaopealbuquerque1@gmail.com>
2023-09-17 17:40:14 -0300

    Adicionando endpoint para obter estados

--- src/main/java/br/com/gohango/domain/model/Estado.java
+++ src/main/java/br/com/gohango/domain/model/Estado.java
@@ -2,6 +2,8 @@ package br.com.gohango.domain.model;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,6 +13,8 @@ import javax.persistence.Id;
 @Entity
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Getter
+@Setter
 public class Estado {
 
     @Id
